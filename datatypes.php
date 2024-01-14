fabd25768ffc891d8f6f0ae71d0d823d14f0604c
ronakumbare <ronakumbare2206@gmail.com>
2024-01-14 16:25:53 +0530

    updates

---
+++ datatypes.php
@@ -0,0 +1,17 @@
+<?php
+$name = "Ronak ";
+$age = 567;
+
+//1 --- string
+$name = "Ronak";
+echo"i am $name <br>";
+//2 --- integer
+$age = 2442;
+echo"age is $age<br>  ";
+//3 --- float - Decimal point number
+$age = 324.3;
+echo "age is $age<br> ";
+
+
+?>
+
--- variables.php
+++ variables.php
@@ -1,3 +1,8 @@
 <?php
-echo "This is variable programs";
+echo "This is variable programs<br>";
+$name = "Ronak";
+$age = 2000000;
+$sal = 99.99;
+
+echo "This is $name his age is $age salary is $sal";
 ?>
