64a306bfda514fa31dfe6b7b8a4b42bdb06c9baa
ronakumbare <ronakumbare2206@gmail.com>
2024-01-24 23:18:43 +0530

    loops addes

--- Loops.php
+++ Loops.php
@@ -1,6 +1,21 @@
 <?php
-for ($i = 0; $i < 5; $i++) 
+// for ($i = 0; $i < 5; $i++) 
+// {
+//     echo "The number is $i <br>";
+// }
+// echo"<br>";
+// echo "----------------------";
+// echo"<br>";
+echo "----------------------";
+echo"<br>";
+echo "While Loop";
+echo"<br>";
+$i = 0;
+while ($i < 10)
 {
-    echo "The number is $i <br>";
+    echo $i+1;
+    echo"<br>";
+    $i++;
 }
+
 ?>
