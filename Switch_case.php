f7b4cec77100d64b8f4e222e0eff11c6ddd5640d
ronakumbare <ronakumbare2206@gmail.com>
2024-01-26 22:19:18 +0530

    Switch added

---
+++ Switch_case.php
@@ -0,0 +1,20 @@
+<?php
+
+$age = 67;
+
+switch ($age)
+{
+    case 12:
+        echo "You are 12 years old<br>";
+        break; 
+    case 67:
+        echo "You are old<br>";
+        break; 
+
+    default:
+        echo "Your age is weird<br>";
+        break;  
+
+}
+
+?>
