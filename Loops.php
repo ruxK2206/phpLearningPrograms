4a75c8bf13fd2c377fcac4b0964037565fd1f376
ronakumbare <ronakumbare2206@gmail.com>
2024-01-28 10:13:23 +0530

    new file added

--- Loops.php
+++ Loops.php
@@ -7,15 +7,33 @@
 // echo "----------------------";
 // echo"<br>";
 echo "----------------------";
-echo"<br>";
-echo "While Loop";
-echo"<br>";
-$i = 0;
-while ($i < 10)
-{
-    echo $i+1;
-    echo"<br>";
-    $i++;
-}
+// echo"<br>";
+// echo "While Loop";
+// echo"<br>";
+// $i = 0;
+// while ($i < 10)
+// {
+//     echo $i+1;
+//     echo"<br>";
+//     $i++;
+// }
 
+// 
+echo "----------------------";
+// echo"<br>";
+// echo "for each Loop";
+// echo"<br>";
+$arr = array("Potato","Bread","eggs","Onion","Capsicum");
+
+// for($i=0;$i < count($arr);$i++)
+// {
+//   echo $arr[$i];
+//   echo "<br>";
+// }
+echo "----------------------";
+echo "<br>";
+foreach ($arr as $value) 
+{
+    echo "$value <br>";
+}
 ?>
